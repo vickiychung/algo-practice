4ca746e79013c6de5df0bed964bf7e76b8f304e5
vickiychung <vickiychung@gmail.com>
2021-06-16 17:28:02 -0700

    Add better solution

--- sortedSquaredArray.js
+++ sortedSquaredArray.js
@@ -1,13 +1,34 @@
+// function sortedSquaredArray(array) {
+//   let squared = [];
+
+//   array.forEach(num => {
+//     squared.push(num ** 2);
+//   })
+
+//   squared.sort(function(a, b){return a - b});
+
+//   return squared;
+// }
+
 function sortedSquaredArray(array) {
-  let squared = [];
+  let sortedSquares = new Array(array.length).fill(0);
+  let smallerValueIdx = 0;
+  let largerValueIdx = array.length - 1;
 
-  array.forEach(num => {
-    squared.push(num ** 2);
-  })
+  for (let i = array.length - 1; i >= 0; i--) {
+    let smallerValue = array[smallerValueIdx];
+    let largerValue = array[largerValueIdx];
 
-  squared.sort(function(a, b){return a - b});
+    if (Math.abs(smallerValue) > Math.abs(largerValue)) {
+      sortedSquares[i] = smallerValue ** 2;
+      smallerValueIdx++;
+    } else {
+      sortedSquares[i] = largerValue ** 2;
+      largerValueIdx--;
+    }
+  }
 
-  return squared;
+  return sortedSquares;
 }
 
 let array = [1, 2, 3, 5, 6, 8, 9];
