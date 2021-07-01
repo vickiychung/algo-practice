a02a9c1f1e831abcd6cc6aaf23861d17299a3bc9
vickiychung <vickiychung@gmail.com>
2021-06-30 21:35:31 -0700

    Account for even chars

--- CtCI/1.4.js
+++ CtCI/1.4.js
@@ -9,10 +9,11 @@ function palindromePermutation(str) {
 
   let filtered = Object.values(count).filter(val => val % 2 !== 0);
 
-  return filtered.length === 1;
+  return filtered.length <= 1;
 }
 
 
-console.log(palindromePermutation("Tact Coa"));
-console.log(palindromePermutation("abc dbca"));
-console.log(palindromePermutation("abcc dbca"));
+console.log(palindromePermutation("Tact Coa")); // true
+console.log(palindromePermutation("abc dbca")); // true
+console.log(palindromePermutation("abcc dbca")); // false
+console.log(palindromePermutation("abccba")); // true
