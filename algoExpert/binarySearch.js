c25063101747b7905ba065d2d2e3484fd293d1fc
vickiychung <vickiychung@gmail.com>
2021-06-30 17:32:42 -0700

    Fix bug

--- algoExpert/binarySearch.js
+++ algoExpert/binarySearch.js
@@ -1,24 +1,15 @@
 function binarySearch(array, target) {
-  // if (array.length <= 1 && target !== array[0]) return -1;
-  
+  if (array.length === 0) return -1;
+
   let mid = Math.floor(array.length / 2);
 
   if (array[mid] === target) {
     return mid;
   } else if (array[mid] > target) {
-    let left = array.slice(0, mid);
-    if (left.length === 1 && target !== left[0]) {
-      return -1;
-    } else {
-      return binarySearch(left, target);
-    }
+    return binarySearch(array.slice(0, mid), target);
   } else {
-    let right = array.slice(mid + 1);
-    if (right.length === 1 && target !== right[0]) {
-      return -1;
-    } else {
-      return binarySearch(right, target) + mid + 1;
-    }
+    let subResult = binarySearch(array.slice(mid + 1), target);
+    return subResult === -1 ? -1 : subResult + mid + 1;
   }
 }
 
