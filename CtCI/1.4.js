50dc72e5401437b6325a4a3c2f53907ea2d73f69
vickiychung <vickiychung@gmail.com>
2021-06-30 21:32:20 -0700

    Solve palindrome permutation

---
+++ CtCI/1.4.js
@@ -0,0 +1,18 @@
+function palindromePermutation(str) {
+  let newStr = str.split(" ").join("").toLowerCase();
+  let count = {};
+
+  for (let i = 0; i < newStr.length; i++) {
+    if (!count[newStr[i]]) count[newStr[i]] = 0;
+    count[newStr[i]] += 1;
+  }
+
+  let filtered = Object.values(count).filter(val => val % 2 !== 0);
+
+  return filtered.length === 1;
+}
+
+
+console.log(palindromePermutation("Tact Coa"));
+console.log(palindromePermutation("abc dbca"));
+console.log(palindromePermutation("abcc dbca"));
