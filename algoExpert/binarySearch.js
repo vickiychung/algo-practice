edee52906af6eb4b0703f93487f3722a0f40ace3
vickiychung <vickiychung@gmail.com>
2021-06-30 17:18:49 -0700

    Initial solution

--- algoExpert/binarySearch.js
+++ algoExpert/binarySearch.js
@@ -1,18 +1,28 @@
 function binarySearch(array, target) {
-  if (array.length === 1 && array[0] === target) return 0;
+  // if (array.length <= 1 && target !== array[0]) return -1;
   
   let mid = Math.floor(array.length / 2);
 
   if (array[mid] === target) {
     return mid;
   } else if (array[mid] > target) {
-    return binarySearch(array.slice(0, mid), target) + 1;
+    let left = array.slice(0, mid);
+    if (left.length === 1 && target !== left[0]) {
+      return -1;
+    } else {
+      return binarySearch(left, target);
+    }
   } else {
-    return binarySearch(array.slice(mid), target) + 1;
+    let right = array.slice(mid + 1);
+    if (right.length === 1 && target !== right[0]) {
+      return -1;
+    } else {
+      return binarySearch(right, target) + mid + 1;
+    }
   }
 }
 
 array = [0, 1, 21, 33, 45, 45, 61, 71, 72, 73];
-target = 33;
+target = 100;
 
 console.log(binarySearch(array, target));
