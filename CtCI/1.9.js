d3c948219066e5f23eb2dee5ba05da56a87e8fda
vickiychung <vickiychung@gmail.com>
2021-07-07 15:51:47 -0700

    Splice substr

--- CtCI/1.9.js
+++ CtCI/1.9.js
@@ -1,6 +1,17 @@
-// function stringRotation() {
+function stringRotation(s1, s2) {
+  // if (!isSubstring(s1, s2)) return false;
 
-// }
+  let subIdx = s1.indexOf(s2);
+  let left = s1.slice(0, subIdx);
+  let right = s1.slice(subIdx + s2.length + 1);
+
+  s1 = s1.split('');
+  s1.splice(subIdx, s2.length);
+  s1 = s1.join('');
+
+  console.log(right);
+  return left;
+}
 
 function isSubstring(str, substr) {
   return str.includes(substr);
@@ -10,3 +21,8 @@ function isSubstring(str, substr) {
 // console.log(isSubstring("waterbottle", "rb")); //true
 // console.log(isSubstring("waterbottle", "elt")); //false
 // console.log(isSubstring("waterbottle", "erbottlewat")); //false
+
+// console.log(stringRotation("waterbottle", "elt")); //false
+// console.log(stringRotation("waterbottle", "rebottletaw")) //false
+// console.log(stringRotation("waterbottle", "bottle")); //false
+console.log(stringRotation("waterbottle", "erbottlewat")); //true
