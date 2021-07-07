0ee20c1b74cb12fb096ec31b19621350e8c11647
vickiychung <vickiychung@gmail.com>
2021-07-07 15:31:14 -0700

    Create helper function

---
+++ CtCI/1.9.js
@@ -0,0 +1,12 @@
+// function stringRotation() {
+
+// }
+
+function isSubstring(str, substr) {
+  return str.includes(substr);
+}
+
+// console.log(isSubstring("waterbottle", "water")); //true
+// console.log(isSubstring("waterbottle", "rb")); //true
+// console.log(isSubstring("waterbottle", "elt")); //false
+// console.log(isSubstring("waterbottle", "erbottlewat")); //false
