a0526171c0fec13c380ce9f9406706c51acbb0e7
vickiychung <vickiychung@gmail.com>
2021-07-09 22:29:45 -0700

    Add is prime

---
+++ structy/002-isPrime.js
@@ -0,0 +1,10 @@
+const isPrime = (n) => {
+  if (n < 2) return false;
+  if (n === 2) return true;
+  
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) return false;
+  }
+  
+  return true;
+};
