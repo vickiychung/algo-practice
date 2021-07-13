31d2e6b261561afd84bdad0a1997e89f7f01a7bc
vickiychung <vickiychung@gmail.com>
2021-07-12 17:35:40 -0700

    Add recursive solution for DFS

--- structy/024-depthFirstValues.js
+++ structy/024-depthFirstValues.js
@@ -1,17 +1,28 @@
 //iterative solution
 
+// const depthFirstValues = (root) => {
+//   if (!root) return [];
+  
+//   let stack = [root];
+//   let output = [];
+
+//   while (stack.length !== 0) {
+//     let curr = stack.pop();
+//     output.push(curr.val);
+//     if (curr.right) stack.push(curr.right);
+//     if (curr.left) stack.push(curr.left);
+//   }
+  
+//   return output;
+// };
+
+//recursive solution
+
 const depthFirstValues = (root) => {
   if (!root) return [];
   
-  let stack = [root];
-  let output = [];
-
-  while (stack.length !== 0) {
-    let curr = stack.pop();
-    output.push(curr.val);
-    if (curr.right) stack.push(curr.right);
-    if (curr.left) stack.push(curr.left);
-  }
+  const leftValues = depthFirstValues(root.left);
+  const rightValues = depthFirstValues(root.right);
   
-  return output;
+  return [root.val, ...leftValues, ...rightValues];
 };
