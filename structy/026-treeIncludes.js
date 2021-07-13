721a15aba22ea4ea3de9c14251f057b2045c659c
vickiychung <vickiychung@gmail.com>
2021-07-13 16:57:30 -0700

    Solve tree includes using BFS

---
+++ structy/026-treeIncludes.js
@@ -0,0 +1,16 @@
+// BFS approach
+const treeIncludes = (root, target) => {
+  if (!root || !target) return false;
+
+  let queue = [root];
+  
+  while (queue.length !== 0) {
+    let curr = queue.shift();
+    if (curr.val === target) return true;
+    if (curr.left) queue.push(curr.left);
+    if (curr.right) queue.push(curr.right);
+  }
+  
+  return false;
+};
+
