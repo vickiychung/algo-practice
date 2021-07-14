4e6a12b9b5144c3b62ca20e5ef04a5b2dfab33ca
vickiychung <vickiychung@gmail.com>
2021-07-13 17:27:54 -0700

    Comment out old code

--- structy/026-treeIncludes.js
+++ structy/026-treeIncludes.js
@@ -1,18 +1,18 @@
 // BFS approach
-const treeIncludes = (root, target) => {
-  if (!root || !target) return false;
+// const treeIncludes = (root, target) => {
+//   if (!root || !target) return false;
 
-  let queue = [root];
+//   let queue = [root];
   
-  while (queue.length !== 0) {
-    let curr = queue.shift();
-    if (curr.val === target) return true;
-    if (curr.left) queue.push(curr.left);
-    if (curr.right) queue.push(curr.right);
-  }
+//   while (queue.length !== 0) {
+//     let curr = queue.shift();
+//     if (curr.val === target) return true;
+//     if (curr.left) queue.push(curr.left);
+//     if (curr.right) queue.push(curr.right);
+//   }
   
-  return false;
-};
+//   return false;
+// };
 
 // DFS recursive approach
 const treeIncludes = (root, target) => {
