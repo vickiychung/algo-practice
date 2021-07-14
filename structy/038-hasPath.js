2816e3508851268168bd5f61cafa231b6903a003
vickiychung <vickiychung@gmail.com>
2021-07-14 11:25:00 -0700

    Solve with BFS

--- structy/038-hasPath.js
+++ structy/038-hasPath.js
@@ -13,3 +13,19 @@ const hasPath = (graph, src, dst) => {
   
   return false;
 };
+
+//BFS
+const hasPath = (graph, src, dst) => {
+  if (graph[src].length === 0) return false;
+  
+  let queue = [graph[src]];
+  
+  while (queue.length !== 0) {
+    let curr = queue.shift();
+    let currValues = Object.values(curr);
+    if (currValues.includes(dst)) return true;
+    currValues.forEach(val => queue.push(graph[val]));
+  }
+  
+  return false;
+};
