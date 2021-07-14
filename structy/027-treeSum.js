77b95837981607f4486625ae1e501d082c857a1b
vickiychung <vickiychung@gmail.com>
2021-07-13 17:09:21 -0700

    Add alternative solution with BFS

--- structy/027-treeSum.js
+++ structy/027-treeSum.js
@@ -1,8 +1,26 @@
+// DFS recursive
 const treeSum = (root) => {
   if (!root) return 0;
   
   let sum = root.val;
   sum += treeSum(root.left) + treeSum(root.right);
+  
+  return sum;
+};
+
+// BFS
+const treeSum = (root) => {
+  if (!root) return 0;
+  
+  let sum = 0;
+  let queue = [root];
+  
+  while (queue.length !== 0) {
+    let curr = queue.shift();
+    sum += curr.val;
+    if (curr.left) queue.push(curr.left);
+    if (curr.right) queue.push(curr.right);
+  }
   
   return sum;
 };
