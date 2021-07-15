ef21bd9bd00b05c7306514529b9e11d7d394a17e
vickiychung <vickiychung@gmail.com>
2021-07-14 17:43:10 -0700

    Comment out alternative solution

--- structy/038-hasPath.js
+++ structy/038-hasPath.js
@@ -1,18 +1,18 @@
 //DFS
-const hasPath = (graph, src, dst) => {
-  if (graph[src].length === 0) return false;
+// const hasPath = (graph, src, dst) => {
+//   if (graph[src].length === 0) return false;
   
-  let stack = [graph[src]];
+//   let stack = [graph[src]];
   
-  while (stack.length !== 0) {
-    let curr = stack.pop();
-    let currValues = Object.values(curr);
-    if (currValues.includes(dst)) return true;
-    currValues.forEach(val => stack.push(graph[val]));
-  }
+//   while (stack.length !== 0) {
+//     let curr = stack.pop();
+//     let currValues = Object.values(curr);
+//     if (currValues.includes(dst)) return true;
+//     currValues.forEach(val => stack.push(graph[val]));
+//   }
   
-  return false;
-};
+//   return false;
+// };
 
 //BFS
 const hasPath = (graph, src, dst) => {
