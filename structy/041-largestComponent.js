5f5c5e1eafbedd278c863f1224f2fb66c3160917
vickiychung <vickiychung@gmail.com>
2021-07-16 22:21:25 -0700

    Add initial solution

---
+++ structy/041-largestComponent.js
@@ -0,0 +1,26 @@
+const largestComponent = (graph) => {
+  if (graph === {}) return 0;
+  
+  let visited = new Set();
+  let max = 0;
+  
+  for (let node in graph) {
+    let traversed = traverse(graph, node, visited);
+    if (traversed > max) max = traversed;
+  }
+  
+  return max;
+};
+
+const traverse = (graph, src, visited) => {
+  if (visited.has(String(src))) return 0;
+  
+  visited.add(String(src));
+  let size = 1;
+  
+  for (let neighbor of graph[src]) {
+    size += traverse(graph, neighbor, visited, size);
+  }
+  
+  return size;
+}
