51ace3e8a059cf007dfa92b4c952088cb5797900
vickiychung <vickiychung@gmail.com>
2021-07-30 16:16:31 -0700

    Solve with memoization

---
+++ structy/052-tribonacci.js
@@ -0,0 +1,9 @@
+const tribonacci = (n, memo = {}) => {
+  if (n === 0 || n === 1) return 0;
+  if (n === 2) return 1;
+  
+  if (n in memo) return memo[n];
+  
+  memo[n] = tribonacci(n - 1, memo) + tribonacci(n - 2, memo) + tribonacci(n - 3, memo);
+  return memo[n];
+};
