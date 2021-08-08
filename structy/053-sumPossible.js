710168feb6c2178cc99ce2536407f2446d38597c
vickiychung <vickiychung@gmail.com>
2021-08-07 18:02:12 -0700

    Solve recursively

---
+++ structy/053-sumPossible.js
@@ -0,0 +1,16 @@
+const sumPossible = (amount, numbers, memo = {}) => {
+  if (amount === 0) return true;
+  if (amount < 0) return false;
+  
+  if (amount in memo) return memo[amount];
+  
+  for (let num of numbers) {
+    if (sumPossible(amount - num, numbers, memo)) {
+      memo[amount] = true;
+      return true;
+    }
+  }
+  
+  memo[amount] = false;
+  return false;
+};
