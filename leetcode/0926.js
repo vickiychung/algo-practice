55bfe1e57c540a504c54ce8555ad95576ebb7762
vickiychung <vickiychung@gmail.com>
2021-08-10 14:36:22 -0700

    Fix with proper results

--- leetcode/0926.js
+++ leetcode/0926.js
@@ -1,36 +1,14 @@
 var minFlipsMonoIncr = function(s) {
-  let min = Infinity;
-  
-  let leftIdx = s.indexOf('1');
-  let left = s.split('').slice(0, leftIdx).join('');
-  let right = s.split('').slice(leftIdx).join('');
-  let count = 0;
-  
-  for (let i = 0; i < left.length; i++) {
-    if (left[i] !== '0') count += 1;
-  }
-  
-  for (let i = 0; i < right.length; i++) {
-    if (right[i] !== '1') count += 1;
-  }
-  
-  if (count < min) min = count;
- 
-  let charCount = {
-    '0': 0,
-    '1': 0
-  };
+  let flipZero = 0; 
+  let flipOne = 0;
   
   for (let i = 0; i < s.length; i++) {
-    if (s[i] === '0') charCount['0'] += 1;
-    if (s[i] === '1') charCount['1'] += 1;
+    if (s[i] === '0') {
+      flipZero = Math.min(flipZero + 1, flipOne);
+    } else {
+      flipOne += 1
+    }
   }
   
-  let diff = charCount['0'] > charCount['1'] ? charCount['1'] : charCount['0'];
-  
-  if (charCount['0'] === charCount['1']) {
-    return min;
-  } else {
-    return min < diff ? min : diff;
-  }
+  return Math.min(flipZero, flipOne);
 };
