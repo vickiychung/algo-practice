2c10aa5a3f63db9630fd8dba4f0516ebc9c00f8a
vickiychung <vickiychung@gmail.com>
2021-08-13 14:07:06 -0700

    Solve with while loop

---
+++ leetcode/0021.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+  let temp = new ListNode(0, null);
+  let curr = temp;
+  
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      curr.next = l1;
+      l1 = l1.next;
+    } else {
+      curr.next = l2;
+      l2 = l2.next;
+    }
+    
+    curr = curr.next;
+  }
+  
+  curr.next = l1 || l2;
+  
+  return temp.next;
+};
