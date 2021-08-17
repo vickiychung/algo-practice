ac6008316607663e5ed00d91600ea6db8b5ecbe2
vickiychung <vickiychung@gmail.com>
2021-08-16 18:42:32 -0700

    Solve iteratively

---
+++ leetcode/0303.js
@@ -0,0 +1,13 @@
+var NumArray = function(nums) {
+  this.nums = nums;
+};
+
+NumArray.prototype.sumRange = function(left, right) {
+  let sum = 0;
+  
+  for (let i = left; i <= right; i++) {
+    sum += this.nums[i];
+  }
+  
+  return sum;
+};
