e1941302a967e429ce16ca3bd277a8ca2b30bf6c
vickiychung <vickiychung@gmail.com>
2021-08-23 15:40:35 -0700

    Account for all cases

--- codeSignal/016.js
+++ codeSignal/016.js
@@ -1,18 +1,26 @@
 function areSimilar(a, b) {
   let count = 0;
+  let tempA = null; 
+  let tempB = null;
   
   for (let i = 0; i < a.length; i++) {
     if (a[i] !== b[i]) {
-      if (count > 0) return false;
-      
-      if (a[i] === b[i+1] && a[i+1] === b[i]) {
-        count += 1;
-        i++;
-      } else {
+      if (count > 0) {
         return false;
+      } else if (tempA && tempB) {
+        if (a[i] === tempB && b[i] === tempA) {
+          count += 1;
+          tempA = null;
+          tempB = null;
+        } else {
+          return false;
+        }
+      } else {
+        tempA = a[i];
+        tempB = b[i];
       }
     }
   }
   
-  if (count <= 1) return true;
+  return true;
 }
