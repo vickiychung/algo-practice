c737ab16b34863f21b0595de22b4df2da646bd5d
vickiychung <vickiychung@gmail.com>
2021-08-29 23:08:41 -0700

    Operate in select

---
+++ codeSignal/databases/26.sql
@@ -0,0 +1,9 @@
+DROP PROCEDURE IF EXISTS legsCount;
+CREATE PROCEDURE legsCount()
+    SELECT (
+      SUM(
+        IF (type = 'human', 2, 4)
+      )
+    ) as summary_legs
+    FROM creatures
+    ORDER BY id;
