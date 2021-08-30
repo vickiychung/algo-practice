fa4e1aeff1ed7adbafe48a4855a7b68f1af8f4b2
vickiychung <vickiychung@gmail.com>
2021-08-29 20:44:19 -0700

    Order by field

---
+++ codeSignal/databases/10.sql
@@ -0,0 +1,12 @@
+CREATE PROCEDURE mischievousNephews()
+BEGIN
+  SELECT WEEKDAY(mischief_date) as weekday,
+    mischief_date,
+    author,
+    title
+  FROM mischief
+  ORDER BY weekday,
+    FIELD(author, 'Huey', "Dewey", 'Louie'),
+    mischief_date,
+    title;
+END
