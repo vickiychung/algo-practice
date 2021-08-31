0220cc44d84441204a18043ab04f2834340f3a67
vickiychung <vickiychung@gmail.com>
2021-08-31 14:24:21 -0700

    Push subarrays to ingredients

---
+++ codeSignal/interviewPractice/hashTables/groupingDishes.js
@@ -0,0 +1,28 @@
+function groupingDishes(dishes) {
+  let dishIng = {};
+  
+  dishes.forEach(dish => {
+    const dishName = dish[0];
+    const ingredients = dish.slice(1);
+    
+    ingredients.forEach(ingredient => {
+      if (!dishIng[ingredient]) dishIng[ingredient] = [];
+      dishIng[ingredient].push(dishName);
+    })
+  })
+  
+  let filteredIng = Object.keys(dishIng).filter(key => dishIng[key].length > 1);
+  filteredIng.sort((a, b) => a.localeCompare(b));
+  
+  let output = [];
+  
+  filteredIng.forEach(ing => {
+    let dishes = dishIng[ing];
+    dishes.sort((a, b) => a.localeCompare(b));
+    
+    let subArr = [ing];
+    output.push(subArr.concat(dishes));
+  })
+  
+  return output;
+}
