418a2b974e62181d1a9fa5a27d32fffd648d47ed
vickiychung <vickiychung@gmail.com>
2021-09-01 15:44:59 -0700

    Use array instead of string for output

--- codeSignal/interviewPractice/heapsStacksQueues/simplifyPath.js
+++ codeSignal/interviewPractice/heapsStacksQueues/simplifyPath.js
@@ -1,24 +1,19 @@
 function simplifyPath(path) {
   if (path.length <= 1) return '/';
   
-  let output = '/';
+  let output = [];
   let arr = path.split('/');
   
-  arr.forEach((path, i) => {
+  arr.forEach(path => {
     if (path !== '' && path !== '.' && path !== '..') {
-      output += path;
-      output += '/';
+      output.push(path);
     } else if (path === '..') {
-      let prevLength = arr[i - 1].length;
-      output = output.substring(0, output.length - 1 - prevLength);
-    } 
-    
-    if (output.length <= 1) output = '/';
+      output.pop();
+    }
   })
   
-  if (output.length > 1 && output[output.length - 1] === '/') {
-    output = output.substring(0, output.length - 1);
-  }
+  output = output.join('/');
+  output = '/' + output;
   
   return output;
 }
